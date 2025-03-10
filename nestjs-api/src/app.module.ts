e368b4d008ef33be228132d02c798c7d0e5b7986
Darlinson Alves <dev.darlinsonalves@gmail.com>
2025-03-10 09:20:18 -0300

    feat: controller assets

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AssetsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:root@localhost:27017/home_broke?authSource=admin&directConnection=true'),
+    AssetsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
